2f4bbcf12c6e5bfbc9bd717fbbd6daa45bdd4df2
msmichova <94367405+msmichova@users.noreply.github.com>
2021-11-22 12:07:28 +0000

    Initial commit with carbon button

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.scss';
+// import { Button } from 'carbon-components-react';
+import { Button } from '../node_modules/carbon-components-react';
+
+function App() {
+  return (
+    <div>
+      <Button>Example usage</Button>
+    </div>  
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
