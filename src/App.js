ce6b8f02f3e581a61f4b5cf9a48f83752268b931
Martina Smichova <smichova-m@ulster.ac.uk>
2021-11-22 17:21:49 +0000

    carbon tutorial: complete step 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.scss';
-// import { Button } from 'carbon-components-react';
-import { Button } from '../node_modules/carbon-components-react';
+import { Button, Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
-function App() {
-  return (
-    <div>
-      <Button>Example usage</Button>
-    </div>  
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <TutorialHeader />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+          <Button>Button</Button>
+        </Content>
+      </>
+    );
+  }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
