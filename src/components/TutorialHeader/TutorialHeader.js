eab73cd57bd2ab74fe69430b7c4ba8eb2c7c10d2
Martina Smichova <smichova-m@ulster.ac.uk>
2021-12-23 10:56:18 +0000

    add sample form

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -24,45 +24,47 @@ import { link } from 'fs';
 const TutorialHeader = () => (
   <HeaderContainer
     render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-      <Header aria-label="Carbon Tutorial">
-        <SkipToContent />
-        <HeaderMenuButton
-          aria-label="Open menu"
-          onClick={onClickSideNavExpand}
-          isActive={isSideNavExpanded}
-        />
-        <HeaderName element={Link} to="/" prefix="IBM">
-          Carbon Tutorial
-        </HeaderName>
-        <HeaderNavigation aria-label="Carbon Tutorial">
-          <HeaderMenuItem element={link} to="/repos">
-            Repositories
-          </HeaderMenuItem>
-        </HeaderNavigation>
-        <SideNav
-          aria-label="Side navigation"
-          expanded={isSideNavExpanded}
-          isPersistent={false}
-        >
-          <SideNavItems>
-            <HeaderSideNavItems>
-              <HeaderMenuItem href="/repos">Repositories</HeaderMenuItem>
-            </HeaderSideNavItems>
-          </SideNavItems>
-        </SideNav>
-        {/* <HeaderGlobalBar /> */}
-        <HeaderGlobalBar>
-          <HeaderGlobalAction aria-label="Notifications">
-            <Notification20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="User Avatar">
-            <UserAvatar20 />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="App Switcher">
-            <AppSwitcher20 />
-          </HeaderGlobalAction>
-        </HeaderGlobalBar>
-      </Header>
+      <>
+        <Header aria-label="Carbon Tutorial">
+          <SkipToContent />
+          <HeaderMenuButton
+            aria-label="Open menu"
+            onClick={onClickSideNavExpand}
+            isActive={isSideNavExpanded}
+          />
+          <HeaderName element={Link} to="/" prefix="IBM">
+            Carbon Tutorial
+          </HeaderName>
+          <HeaderNavigation aria-label="Carbon Tutorial">
+            <HeaderMenuItem element={link} to="/repos">
+              Repositories
+            </HeaderMenuItem>
+          </HeaderNavigation>
+          <SideNav
+            aria-label="Side navigation"
+            expanded={isSideNavExpanded}
+            isPersistent={false}
+          >
+            <SideNavItems>
+              <HeaderSideNavItems>
+                <HeaderMenuItem href="/repos">Repositories</HeaderMenuItem>
+              </HeaderSideNavItems>
+            </SideNavItems>
+          </SideNav>
+          {/* <HeaderGlobalBar /> */}
+          <HeaderGlobalBar>
+            <HeaderGlobalAction aria-label="Notifications">
+              <Notification20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="User Avatar">
+              <UserAvatar20 />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction aria-label="App Switcher">
+              <AppSwitcher20 />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+        </Header>
+      </>
     )}
   />
 );
--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,5 +1,16 @@
+import { TextArea } from 'carbon-components-react';
 import React from 'react';
 
-const LandingPage = () => <div>LANDING PAGE</div>;
+const LandingPage = () => (
+  <>
+    <div>LANDING PAGE</div>
+    <TextArea labelText="Question 1" placeholder="Your answer here..." />
+    <br />
+    <TextArea labelText="Question 2" placeholder="Your answer here..." />
+    <br />
+    <TextArea labelText="Question 3" placeholder="Your answer here..." />
+    <br />
+  </>
+);
 
 export default LandingPage;
