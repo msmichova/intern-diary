49c09a05006783c27d2fe1693d6b622c940e3825
Martina Smichova <smichova-m@ulster.ac.uk>
2022-05-22 15:01:24 +0100

    credential variable linting error

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const micosoftLogin = () => {
       // IdP data available in result.additionalUserInfo.profile.
       /** @type {firebase.auth.OAuthCredential} */
       const { credential } = result;
-      console.log({ result });
+      console.log({ credential });
 
       // OAuth access and id tokens can also be retrieved:
       // const { accessToken } = credential;
